0235c9e282100b3ce22c03e842ba502a4852e233
Maui Bautista <mherdy_2@hotmail.com>
2023-04-19 23:09:24 -0700

    block13

---
+++ greater-than-5.js
@@ -0,0 +1,14 @@
+//  num1 and num2 are values to test
+let num1 = 15;
+let num2 = 10;
+
+// if num 2 is greater than num1 "true" will print
+if (num2 > num1) {
+  console.log(true);
+
+  //  if num 1 is greater than num2 "true" will print
+} else if (num1 > num2) {
+  console.log(false);
+} else if (num1 === num2) {
+  console.log(true);
+}
--- number-line.js
+++ number-line.js
@@ -1,14 +1,20 @@
-let num1 = 0;
-let num2 = 0;
+let num1 = 50;
+let num2 = -50;
+let i = 0;
 
 let sum = num1 + num2;
 // console.log(sum);
 
 if (sum > 100) {
+  // if the result of sum is greater than 100 this will print
   console.log(sum + " is greater than 100");
-} else if (sum < 100) {
+}
+// if the result of sum is less than 100 this will print
+else if (sum < 100) {
   console.log(sum + " is greater than 0");
-} else if (sum >== 0) {
+}
+//  if the result of sum is equal to zero this print
+else if (i === sum) {
   console.log(sum + " 0 is equal to 0");
 }
 // else if (sum === -0) {
