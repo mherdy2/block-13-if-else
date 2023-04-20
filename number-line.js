051db2ee47098cbd0b4a97c35e463ca7765d8e01
Maui Bautista <mherdy_2@hotmail.com>
2023-04-19 22:27:43 -0700

    block 13

---
+++ number-line.js
@@ -0,0 +1,16 @@
+let num1 = 0;
+let num2 = 0;
+
+let sum = num1 + num2;
+// console.log(sum);
+
+if (sum > 100) {
+  console.log(sum + " is greater than 100");
+} else if (sum < 100) {
+  console.log(sum + " is greater than 0");
+} else if (sum >== 0) {
+  console.log(sum + " 0 is equal to 0");
+}
+// else if (sum === -0) {
+//  console.log ()
+// }
