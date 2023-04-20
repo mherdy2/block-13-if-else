8780fad301ca0627afb470507a785db24c8fd650
Maui Bautista <mherdy_2@hotmail.com>
2023-04-20 06:10:08 -0700

    block13

--- pair-and-compare.js
+++ pair-and-compare.js
@@ -1,11 +1,8 @@
-const = obj1
-{
-param1A :"cat",
-param1B :"cat"
+const obj1 = {
+  param1A: "cat",
+  param1B: "cat",
 };
-const = obj2
-{
-param2A : 6,
-param2B : "6"
+const obj2 = {
+  param2A: 6,
+  param2B: "6",
 };
-
