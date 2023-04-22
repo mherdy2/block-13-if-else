60ee983c78a75bd462464be3c6a510610f2f4063
Maui Bautista <mherdy_2@hotmail.com>
2023-04-21 22:14:52 -0700

    edits

--- greater-than-5.js
+++ greater-than-5.js
@@ -1,14 +1,11 @@
 //  num1 and num2 are values to test
-let num1 = 5;
-let num2 = 5;
-// if num 2 is greater than num1 "true" will print
-if (num2 > num1) {
+let num1 = 1000;
+let num2 = -1000;
+// if num1 is less than num1 "true" will print
+if (num1 <= num2) {
   console.log(true);
-  //  if num 1 is greater than num2 "true" will print
-} else if (num1 > num2) {
-  //  if num 1 is greater than num2 "false" will print
+  //  if num1 is greater than num2 "true" will print
+} else if (num1 >= num2) {
+  //  if num1 is greater than num2 "false" will print
   console.log(false);
-} else if (num1 === num2) {
-  //  if num 1 is equal to num2 "true" will print
-  console.log(true);
 }
--- is-truthy.js
+++ is-truthy.js
@@ -3,32 +3,26 @@
 
 // enter input values to be tested ("I am a string", false, null, undefined, 0, "")
 let test = false;
-
 // this will print true
 if (test === "I am a string") {
   console.log("True");
 }
-
 // this will print "The boolean value false is falsy"
 else if (test === false) {
   console.log("The boolean value false is falsy");
 }
-
 // this will print "The null value is falsy"
 else if (test === null) {
   console.log("The null value is falsy");
 }
-
 // this will print "undefined is falsy"
 else if (test === undefined) {
   console.log("undefined is falsy");
 }
-
 // this will print "The number 0 is falsy (the only falsy number)";
 else if (test === 0) {
   console.log("The number 0 is falsy (the only falsy number)");
 }
-
 // this will print "The empty string is falsy (the only falsy string)";
 else if (test === "") {
   console.log("The empty string is falsy (the only falsy string");
