a6e2e7100549d904d4b81ac374fcaea689be298f
Maui Bautista <mherdy_2@hotmail.com>
2023-04-21 21:46:58 -0700

    pair and compare

--- greater-than-5.js
+++ greater-than-5.js
@@ -1,11 +1,9 @@
 //  num1 and num2 are values to test
-let num1 = 5;
-let num2 = 6;
-
+let num1 = 10;
+let num2 = 11;
 // if num 2 is greater than num1 "true" will print
 if (num2 > num1) {
   console.log(true);
-
   //  if num 1 is greater than num2 "true" will print
 } else if (num1 > num2) {
   console.log(false);
--- number-line.js
+++ number-line.js
@@ -1,24 +1,18 @@
-const num1 = 500;
-const num2 = -500;
+const num1 = -5;
+const num2 = 0;
 let i = 0;
 
 let sum = num1 + num2;
-// console.log(sum);
-
 if (sum > 100) {
   // if the result of sum is greater than 100 this will print
   console.log(sum + " is greater than 100");
-}
-// if the result of sum is less than 100 this will print
-else if (sum < 100) {
+} else if (sum === i) {
+  //  if the result of sum is equal to zero this print
+  console.log(i + "  is equal to 0");
+} else if (num1 < num2) {
+  // if num1 is a negative number this will print
+  console.log(num1 + " is a negative number");
+} else if (sum < 100) {
+  //if the result of sum is less than 100 this will print
   console.log(sum + " is greater than 0");
 }
-//  if the result of sum is equal to zero this print
-else if (i === sum) {
-  console.log(i + "  is equal to 0");
-} else if (-1000 < i) {
-  console.log("-1000 is a negative number");
-}
-
-// let sum = num1 + num2;
-if (sum === i) console.log(sum + " is equal to 0");
