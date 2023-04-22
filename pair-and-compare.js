9ddc8533123c26edd8d8c088347c6470a25a4f96
Maui Bautista <mherdy_2@hotmail.com>
2023-04-21 23:51:18 -0700

    block13 done

--- pair-and-compare.js
+++ pair-and-compare.js
@@ -2,7 +2,9 @@ let param1A = 11,
   param1B = "eleven",
   param2A = "four",
   param2B = "for";
-
+// param1A should be strictly equal to param1B
+// while param2A and param2B should be loose equal to each other
+// and if one of the values are false it will return false otherwise true
 if (param1A === param1B && param2A == param2B) {
   console.log(true);
 } else {
