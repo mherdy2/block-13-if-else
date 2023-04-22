de4ba55c3b24f7da969265dfb4638c864fa31658
Maui Bautista <mherdy_2@hotmail.com>
2023-04-21 23:40:37 -0700

    block13 done

--- is-truthy.js
+++ is-truthy.js
@@ -1,29 +1,24 @@
 // truthy and falsy
 //  if else statements
 
-// enter input values to be tested ("I am a string", false, null, undefined, 0, "")
+// input values to be tested ("I am a string", false, null, undefined, 0, "")
 let test = false;
-// this will print true
 if (test === "I am a string") {
+  // this will print true
   console.log("True");
-}
-// this will print "The boolean value false is falsy"
-else if (test === false) {
+} else if (test === false) {
+  // this will print "The boolean value false is falsy"
   console.log("The boolean value false is falsy");
-}
-// this will print "The null value is falsy"
-else if (test === null) {
+} else if (test === null) {
+  // this will print "The null value is falsy"
   console.log("The null value is falsy");
-}
-// this will print "undefined is falsy"
-else if (test === undefined) {
+} else if (test === undefined) {
+  // this will print "undefined is falsy"
   console.log("undefined is falsy");
-}
-// this will print "The number 0 is falsy (the only falsy number)";
-else if (test === 0) {
+} else if (test === 0) {
+  // this will print "The number 0 is falsy (the only falsy number)";
   console.log("The number 0 is falsy (the only falsy number)");
-}
-// this will print "The empty string is falsy (the only falsy string)";
-else if (test === "") {
+} else if (test === "") {
+  // this will print "The empty string is falsy (the only falsy string)";
   console.log("The empty string is falsy (the only falsy string");
 }
