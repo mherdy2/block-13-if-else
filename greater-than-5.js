537e50f5120046024595c58a74993066fc0f1ed0
Maui Bautista <mherdy_2@hotmail.com>
2023-04-21 21:52:16 -0700

    pair and compare

--- greater-than-5.js
+++ greater-than-5.js
@@ -1,12 +1,14 @@
 //  num1 and num2 are values to test
-let num1 = 10;
-let num2 = 11;
+let num1 = 5;
+let num2 = 5;
 // if num 2 is greater than num1 "true" will print
 if (num2 > num1) {
   console.log(true);
   //  if num 1 is greater than num2 "true" will print
 } else if (num1 > num2) {
+  //  if num 1 is greater than num2 "false" will print
   console.log(false);
 } else if (num1 === num2) {
+  //  if num 1 is equal to num2 "true" will print
   console.log(true);
 }
